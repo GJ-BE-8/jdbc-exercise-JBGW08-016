9bd2bc2ccf9d8f803d4ca3f2cabaf4a36f7c64e2
gimzaezin <gimzaezin@gmail.com>
2024-10-23 13:18:35 +0900

    commit

--- src/main/java/com/nhnacademy/jdbc/util/DbUtils.java
+++ src/main/java/com/nhnacademy/jdbc/util/DbUtils.java
@@ -13,7 +13,7 @@ public class DbUtils {
         Connection connection = null;
         try {
             //todo connection.
-            connection = DriverManager.getConnection("jdbc:mysql://133.186.241.167:3306/nhn_academy_0","nhn_academy_0","1j0OTaJgkS1@$dWl");
+            connection = DriverManager.getConnection("jdbc:mysql://133.186.241.167:3306/nhn_academy_16","nhn_academy_16","ANQDj5vXfM@1vTo@");
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
