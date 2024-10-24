2ae73a3a7f925282c3cc7f743ea6c4bb052b3dd4
gimzaezin <gimzaezin@gmail.com>
2024-10-24 15:50:38 +0900

    commit

--- src/main/java/com/nhnacademy/jdbc/student/repository/impl/StudentRepositoryImpl.java
+++ src/main/java/com/nhnacademy/jdbc/student/repository/impl/StudentRepositoryImpl.java
@@ -14,19 +14,19 @@ import java.util.Optional;
 public class StudentRepositoryImpl implements StudentRepository {
 
     @Override
-    public int save(Connection connection, Student student){
+    public int save(Connection connection, Student student) {
         String sql = "insert into jdbc_students(id,name,gender,age) values(?,?,?,?)";
 
-        try(
-            PreparedStatement statement = connection.prepareStatement(sql);
-        ){
+        try (
+                PreparedStatement statement = connection.prepareStatement(sql);
+        ) {
             statement.setString(1, student.getId());
             statement.setString(2, student.getName());
             statement.setString(3, student.getGender().toString());
-            statement.setInt(4,student.getAge());
+            statement.setInt(4, student.getAge());
 
             int result = statement.executeUpdate();
-            log.debug("save:{}",result);
+            log.debug("save:{}", result);
             return result;
         } catch (SQLException e) {
             throw new RuntimeException(e);
@@ -35,18 +35,18 @@ public class StudentRepositoryImpl implements StudentRepository {
     }
 
     @Override
-    public Optional<Student> findById(Connection connection,String id){
+    public Optional<Student> findById(Connection connection, String id) {
         String sql = "select * from jdbc_students where id=?";
-        log.debug("findById:{}",sql);
+        log.debug("findById:{}", sql);
 
         ResultSet rs = null;
-        try(
-            PreparedStatement statement = connection.prepareStatement(sql);
+        try (
+                PreparedStatement statement = connection.prepareStatement(sql);
         ) {
-            statement.setString(1,id);
+            statement.setString(1, id);
             rs = statement.executeQuery();
-            if(rs.next()){
-                Student student =  new Student(rs.getString("id"),
+            if (rs.next()) {
+                Student student = new Student(rs.getString("id"),
                         rs.getString("name"),
                         Student.GENDER.valueOf(rs.getString("gender")),
                         rs.getInt("age"),
@@ -57,7 +57,7 @@ public class StudentRepositoryImpl implements StudentRepository {
         } catch (SQLException e) {
             e.printStackTrace();
             throw new RuntimeException(e);
-        }finally {
+        } finally {
             try {
                 rs.close();
             } catch (SQLException e) {
@@ -68,21 +68,21 @@ public class StudentRepositoryImpl implements StudentRepository {
     }
 
     @Override
-    public int update(Connection connection,Student student){
+    public int update(Connection connection, Student student) {
         String sql = "update jdbc_students set name=?, gender=?, age=? where id=?";
-        log.debug("update:{}",sql);
+        log.debug("update:{}", sql);
 
-        try(
-            PreparedStatement statement = connection.prepareStatement(sql);
+        try (
+                PreparedStatement statement = connection.prepareStatement(sql);
         ) {
-            int index=0;
+            int index = 0;
             statement.setString(++index, student.getName());
             statement.setString(++index, student.getGender().toString());
             statement.setInt(++index, student.getAge());
             statement.setString(++index, student.getId());
 
             int result = statement.executeUpdate();
-            log.debug("result:{}",result);
+            log.debug("result:{}", result);
             return result;
         } catch (SQLException e) {
             throw new RuntimeException(e);
@@ -90,15 +90,15 @@ public class StudentRepositoryImpl implements StudentRepository {
     }
 
     @Override
-    public int deleteById(Connection connection,String id){
+    public int deleteById(Connection connection, String id) {
         String sql = "delete from jdbc_students where id=?";
 
-        try(
-            PreparedStatement statement = connection.prepareStatement(sql);
+        try (
+                PreparedStatement statement = connection.prepareStatement(sql);
         ) {
             statement.setString(1, id);
             int result = statement.executeUpdate();
-            log.debug("result:{}",result);
+            log.debug("result:{}", result);
             return result;
         } catch (SQLException e) {
             throw new RuntimeException(e);
@@ -109,11 +109,11 @@ public class StudentRepositoryImpl implements StudentRepository {
     public int deleteAll(Connection connection) {
         String sql = "delete from jdbc_students";
 
-        try(
+        try (
                 PreparedStatement statement = connection.prepareStatement(sql);
         ) {
             int result = statement.executeUpdate();
-            log.debug("result:{}",result);
+            log.debug("result:{}", result);
             return result;
         } catch (SQLException e) {
             throw new RuntimeException(e);
@@ -123,13 +123,70 @@ public class StudentRepositoryImpl implements StudentRepository {
     @Override
     public long totalCount(Connection connection) {
         //todo#4 totalCount 구현
-        return 0l;
+        String sql = "SELECT COUNT(*) FROM jdbc_students";
+        try (
+                PreparedStatement statement = connection.prepareStatement(sql);
+                ResultSet rs = statement.executeQuery();
+        ) {
+            if (rs.next()) {
+                long count = rs.getLong(1);
+                log.debug("Total count: {}", count);
+                return count;
+            }
+        } catch (SQLException e) {
+            log.error("Error executing totalCount", e);
+            throw new RuntimeException(e);
+        }
+
+        return 0L;
     }
 
     @Override
     public Page<Student> findAll(Connection connection, int page, int pageSize) {
         //todo#5 페이징 처리 구현
-        return null;
+        int offset = (page-1) * pageSize;
+        int limit = pageSize;
+
+        ResultSet rs = null;
+        String sql="select id, name, gender, age, created_at from jdbc_students order by id desc limit  ?,? ";
+        try(PreparedStatement psmt = connection.prepareStatement(sql)) {
+            psmt.setInt(1,offset);
+            psmt.setInt(2,limit);
+            rs= psmt.executeQuery();
+            List<Student> studentList = new ArrayList<>(pageSize);
+
+            while(rs.next()){
+                studentList.add(
+                        new Student(
+                                rs.getString("id"),
+                                rs.getString("name"),
+                                Student.GENDER.valueOf(rs.getString("gender")),
+                                rs.getInt("age"),
+                                rs.getTimestamp("created_at").toLocalDateTime()
+                        )
+                );
+            }
+
+            long total =0;
+
+            if(!studentList.isEmpty()){
+                // size>0 조회 시도, 0이면 조회할 필요 없음, count query는 자원을 많이 소모하는 작업
+                total = totalCount(connection);
+            }
+
+            return  new Page<Student>(studentList,total);
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }finally {
+            try {
+                if(Objects.nonNull(rs)){
+                    rs.close();
+                }
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 
 }
